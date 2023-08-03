f3d7e34478e89ecb7802bc9d714578d02ac935d6
Sebastian Rojas <sebastianrojas812@gmail.com>
2023-08-02 22:52:17 -0400

    init setup, prisma, next-auth, shadcn-ui

---
+++ lib/nextauth.ts
@@ -0,0 +1,55 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { DefaultSession, NextAuthOptions } from "next-auth";
+import { prisma } from "./db";
+import GoogleProvider from 'next-auth/providers/google';
+
+
+declare module 'next-auth' {
+    interface Session extends DefaultSession {
+        user: {
+            id: string;
+        } & DefaultSession['user']
+    }
+}
+
+declare module 'next-auth/jwt' {
+    interface JWT {
+        id: string;
+    }
+}
+
+export const authOptions: NextAuthOptions = {
+    session: {
+        strategy: 'jwt'
+    },
+    secret: process.env.NEXTAUTH_SECRET || '',
+    adapter: PrismaAdapter(prisma),
+    callbacks: {
+        jwt: async ({ token }) => {
+            const db_user = await prisma.user.findFirst({
+                where: {
+                    email: token.email
+                }
+            });
+            if (db_user) {
+                token.id = db_user.id
+            }
+            return token;
+        },
+        session: ({ session, token }) => {
+            if (token) {
+                session.user.id = token.id;
+                session.user.name = token.name;
+                session.user.email = token.email
+                session.user.image = token.picture;
+            }
+            return session;
+        }
+    },
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        })
+    ]
+}
